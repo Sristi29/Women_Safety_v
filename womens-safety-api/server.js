4fc71707acbd4247353404be9f4b98732d3b04d1
Sristi Srivastava <sristidpsbsc@gmail.com>
2025-06-24 23:13:38 +0530

    Added tests, updated README, and included coverage screenshot

--- womens-safety-api/server.js
+++ womens-safety-api/server.js
@@ -5,23 +5,21 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-
-
-// Middleware
 app.use(express.json());
 
-// Routes
 const reportRoutes = require('./routes/reportRoutes');
 app.use('/api/reports', reportRoutes);
 
+module.exports = app;
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('MongoDB connected');
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-  });
-}).catch(err => console.log(err));
+if (require.main === module) {
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => {
+    console.log('MongoDB connected');
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  }).catch(err => console.log(err));
+}
